d73c0fcc84f5a58389be0cded40de19a02f3850d
Wim Boucquaert <wim.boucquaert@gmail.com>
2012-08-09 16:20:24 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2002-2010 Infrae. All rights reserved.
+# Copyright (c) 2002-2012 Infrae. All rights reserved.
 # $Id$
 
 from setuptools import setup, find_packages
+import os
 
 version = '1.0dev'
 
 setup(name='silvatheme.silvacmsorg',
       version=version,
       description="silvacms.org website Skin",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
@@ -25,6 +28,7 @@ setup(name='silvatheme.silvacmsorg',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'grokcore.chameleon',
           'silva.core.layout',
           'silva.core.views',
           'silva.core.conf',

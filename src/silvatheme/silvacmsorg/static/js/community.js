6e8241d2c0e22f1b9281bc108e657fe7ccc81ec1
Sanne Veroude <info@infrae.com>
2012-11-23 15:16:18 +0100

    changes to js, font sizes and screenshots

--- src/silvatheme/silvacmsorg/static/js/community.js
+++ src/silvatheme/silvacmsorg/static/js/community.js
@@ -37,6 +37,36 @@
 	  $("#dark").click( function(){ $
 		  ("#community #tabs li a").removeClass("tabsLight")
 	  });
+	  
+	  $('.menu').stickyfloat();
+			$('.menu2').stickyfloat('update',{ duration : 0 });
+			
+			// after page refresh, make sure the values are returned to their defaults
+			$('.menu :text').each(function(){
+				$(this).val(this.defaultValue);
+			});
+			
+			$('.menu :checkbox').on('change', function(){
+				var elem = $(this).parents('.menu'),
+					prop = this.className,
+					options = {};
+	
+				options[prop] = this.checked ? true : false;
+				elem.stickyfloat('update', options);
+			});
+
+			$('.menu .cssTransition:checkbox').on('change', function(){
+				var elem = $(this).parents('.menu'),
+					val = this.checked ? 0 : elem.find('.duration').val();
+				
+				elem.toggleClass('transition200');
+				elem.stickyfloat('update',{cssTransition: this.checked});
+			});
+			
+			$('.menu :checkbox').each(function(){
+				$(this).prop('checked', this.defaultChecked);
+			});
+
 	    
 	    
     });  

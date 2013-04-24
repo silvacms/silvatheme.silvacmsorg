1bbcc7de319c58c351fbf68b67c961b26f425a35
Wim Boucquaert <wim.boucquaert@gmail.com>
2013-04-24 13:14:29 +0200

    this tab js is used for slideshow

--- src/silvatheme/silvacmsorg/static/js/tabs.js
+++ src/silvatheme/silvacmsorg/static/js/tabs.js
@@ -2,8 +2,7 @@
     $(document).ready(function(){
         $('.tabs ul li a').click(function(){
             var $link = $(this);
-            var anchor = $link.val().split('#');
-            var tab = '#' + anchor.slice(-1);
+            var tab = $link.attr('href');
             $('.tabs ul li').removeClass('selected');
             $link.parent().addClass('selected');
             $('.tabs section').removeClass('selected');

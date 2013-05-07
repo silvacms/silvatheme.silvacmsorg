bf37210653cb84a38b7ae2621c2cc977cc8a7fea
Wim Boucquaert <wim.boucquaert@gmail.com>
2013-05-07 13:29:59 +0200

    add truncation of long lists

--- src/silvatheme/silvacmsorg/static/js/silvacmsorg.js
+++ src/silvatheme/silvacmsorg/static/js/silvacmsorg.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
 
+  // toggle bar
   $("#toggleButton").click(function () {
     $("#toggleBar").toggle('slow');
   });
-    
-} 
+
+  // truncate list
+  var moreLink = $('<li class="show-all"><a href="">Show More</a></li>');
+
+  // hiding all list items after the first 5
+  $('ul.truncate-list').children('li:gt(4)').hide();
+
+  // adding the 'Show More' link
+  $('ul.truncate-list').append(moreLink);
+
+  // binding a click event to 'Show More'
+  moreLink.find('a').on('click', function (){
+
+     // hiding 'Show More' and showing the rest of the list items in this ul
+     $(this).parent().hide().siblings('li').show();
+
+     // preventing default action and event bubbling
+     return false;
+  });
+
+}
+
+
